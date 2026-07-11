bc286ffef01232f1dfdd4f95871131deaad64b20
mint0326 <jmj200436@hansung.ac.kr>
2026-07-11 18:54:17 +0900

    refact: applayout 적용 및 개발용 컴포넌트 페이지 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,7 @@
-﻿import { PagePlaceholder } from '@/shared/ui';
+import { AppLayout, PagePlaceholder } from '@/shared/ui';
 
 export const LoginPage = () => (
-  <main className="min-h-screen bg-stology-off-white px-6">
+  <AppLayout>
     <PagePlaceholder code="LGN001" title="로그인" />
-  </main>
+  </AppLayout>
 );
